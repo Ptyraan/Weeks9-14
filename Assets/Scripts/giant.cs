adc6febaa16e36c3621deeff6fb599eeb98d08cf
pangta <pangta@OA-S141-02>
2026-04-01 10:59:33 -0400

    giant challenge done

---
+++ Assets/Scripts/giant.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
+
+public class giant : MonoBehaviour
+{
+    
+}
--- Assets/Scripts/knight.cs
+++ Assets/Scripts/knight.cs
@@ -1,22 +1,84 @@
+using UnityEditor.Rendering;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class knight : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public AudioClip[] footsteps;
+    public Grid tilemap;
+    public GameObject idle;
+    public Camera camera;
+    public float shook = 0;
+    Vector3 scale;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        scale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = tilemap.transform.position;
+
+
+        if (Keyboard.current.wKey.isPressed)
+        {
+            pos.y -= 0.05f;
+            scale.x = 3;
+        }
+        if (Keyboard.current.sKey.isPressed)
+        {
+            pos.y += 0.05f;
+            scale.x = -3;
+        }
+        if (Keyboard.current.dKey.isPressed)
+        {
+            pos.x -= 0.05f;
+            scale.x = 3;
+        }
+        if (Keyboard.current.aKey.isPressed)
+        {
+            pos.x += 0.05f;
+            scale.x = -3;
+        }
+        if (Keyboard.current.anyKey.isPressed)
+        {
+            transform.localScale = scale;
+            idle.transform.localScale = Vector3.one * 0;
+            scale = transform.localScale;
+        }
+        else
+        {
+            transform.localScale = Vector3.one * 0;
+            idle.transform.localScale = scale;
+            scale = idle.transform.localScale;
+        }
+        tilemap.transform.position = pos;
+        Vector3 CameraPos = Vector3.zero;
+        CameraPos.z = -10;
+        if (shook > 0)
+        {
+            CameraPos.y = 0.2f;
+            camera.transform.position = CameraPos;
+        }
+        else 
+        {
+            camera.transform.position = CameraPos;
+        }
+        shook -= Time.deltaTime;
+
     }
 
     public void Footstep()
     {
-        AudioSource.PlayClipAtPoint(footsteps[Random.Range(0, 9)], transform.position);
+        if (Keyboard.current.anyKey.isPressed)
+        {
+            AudioSource.PlayClipAtPoint(footsteps[Random.Range(0, 9)], transform.position);
+            shook = 0.2f;
+        }
+    
     }
 }
