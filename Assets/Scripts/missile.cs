8d3af9ecd043ecbdde22f4f4d7ef2f7a7789ee5e
Ptyraan <168381801+Ptyraan@users.noreply.github.com>
2026-04-07 18:25:31 -0400

    missile trackings work

--- Assets/Scripts/missile.cs
+++ Assets/Scripts/missile.cs
@@ -4,34 +4,55 @@ public class missile : MonoBehaviour
 {
     public helicopterphysics tgt;
     public float heading;
+    public Vector3 velocity;
+    public float tgtAngle;
+    public int active;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float tgtAngle = Mathf.Atan2(transform.position.y - tgt.transform.position.y, transform.position.x - tgt.transform.position.x);
-        if ((tgtAngle - heading) * Mathf.Rad2Deg > Time.deltaTime)
+        if (active == 1)
         {
-            heading += Time.deltaTime;
+            tgtAngle = Mathf.Atan2(transform.position.y - tgt.transform.position.y, transform.position.x - tgt.transform.position.x);
+            if ((tgtAngle - heading) * Mathf.Rad2Deg > Time.deltaTime * 10)
+            {
+                heading += Time.deltaTime * Mathf.Deg2Rad * 19;
+            }
+            else if ((tgtAngle - heading) * Mathf.Rad2Deg < -Time.deltaTime * 10)
+            {
+                heading -= Time.deltaTime * Mathf.Deg2Rad * 10;
+            }
+            else
+            {
+                heading = tgtAngle;
+            }
+            velocity = Vector3.zero;
+            velocity.x = -Mathf.Cos(heading) * Time.deltaTime;
+            velocity.y = -Mathf.Sin(heading) * Time.deltaTime;
+            transform.position += velocity;
         }
-        else if ((tgtAngle - heading) * Mathf.Rad2Deg < -Time.deltaTime)
+        else if (active == 2) 
         {
-            heading -= Time.deltaTime;
-        }
-        else
+            Vector3 pos = transform.position;
+            pos.y -= Time.deltaTime;
+            transform.position = pos;
+        } 
+        else if (Vector2.Distance(transform.position, tgt.transform.position) < 9)
         {
-            heading = tgtAngle;
+            active = 1;
         }
-        Vector3 velocity = Vector3.zero;
-        velocity.x = Mathf.Cos(heading) * 0.3f * Time.deltaTime;
-        velocity.y = Mathf.Sin(heading) * 0.3f * Time.deltaTime;
-        transform.position += velocity;
         Vector3 angle = Vector3.zero;
-        angle.z = heading;
-        transform.eulerAngles = angle; 
+        angle.z = heading * Mathf.Rad2Deg + 180;
+        transform.eulerAngles = angle;
+
+        if (active == 1 && (transform.position.y < 0 || Vector2.Distance(transform.position, tgt.transform.position) > 10)) 
+        {
+            active = 2;
+        }
     }
 }
